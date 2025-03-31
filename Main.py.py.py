dcb1d3d6a8c67d86014673b659b6940fd45dbbf5
Braxton Hartley <brxtnhartley@gmail.com>
2025-03-31 13:09:01 -0500

    h

---
+++ Main.py.py.py
@@ -0,0 +1,22 @@
+def main():
+    pass
+    
+def get_shift():
+    pass
+    
+def choose_option():
+    pass
+    
+def get_message():
+    pass
+    
+def create_key(shift):
+    pass
+    
+def encode(message, key):
+    pass
+    
+def decode(message, key):
+    pass
+    
+    
