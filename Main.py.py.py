0069125011d72576e2cf2c884af934505f35ba9b
Braxton Hartley <brxtnhartley@gmail.com>
2025-04-01 13:56:38 -0500

    Update Main.py.py.py

--- Main.py.py.py
+++ Main.py.py.py
@@ -1,4 +1,6 @@
 alphabet = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'y':23, 'x':24, 'z':25}
+shift = 1
+
 def main():
     pass
     
@@ -12,12 +14,18 @@ def get_message(): #jamie
     pass
     
 def create_key(shift):
-    pass
-    
+    num = 0
+    key = {}
+    for item in alphabet:
+        key[item] = num + shift
+        num += 1
+    return key
 def encode(message, key): #jamie
     pass
     
 def decode(message, key):
-    pass
-    
+    alphabet = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'y':23, 'x':24, 'z':25}
+    new_message = ''
+    for item in alphabet:
+        
     
