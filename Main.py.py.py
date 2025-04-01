f0d9d2207ed6aa33868bfaa1ce496a22abf6108e
JamieDuty <jamiedennisduty@gmail.com>
2025-04-01 13:32:54 -0500

    Update Main.py.py.py

--- Main.py.py.py
+++ Main.py.py.py
@@ -1,19 +1,31 @@
+alphabet = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'y':23, 'x':24, 'z':25}
 def main():
     pass
     
 def get_shift(): #jamie
+    #get_shift will prompt the user for the shift value and return the value as a string
+    #it will use variable 'shift_value' as the value
     pass
     
 def choose_option(): #jamie
+    #choose_option will prompt the user to choose to encode or decode
+    #it will return true if the user chooses encode and false if the user chooses decode
+    #validate to only accept the appropriate prompts for encode or decode
     pass
     
-def get_message(): #jamie 
+def get_message(): #jamie
+    #get_message should prompt the user to enter a message that will be encoded or decoded
+    #it should return that message
+    #it will be variable 'user_message'
     pass
     
 def create_key(shift):
     pass
     
 def encode(message, key): #jamie
+    #encode will accept message as a string and key as a dictionary
+    #it will encode the message using the key and return the encoded message as string
+    #the encoded message will be variable 'encoded_message'
     pass
     
 def decode(message, key):
