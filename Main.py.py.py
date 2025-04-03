b763d57fae9cc9b5e7f494493a7589d7add357d4
Braxton Hartley <brxtnhartley@gmail.com>
2025-04-03 13:52:06 -0500

    Update Main.py.py.py

--- Main.py.py.py
+++ Main.py.py.py
@@ -27,14 +27,11 @@ def decode(message, key):
     alphabet = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16, 'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'y':23, 'x':24, 'z':25}
     new_message = ''
     new_key = {}
-    print(message)
     diff = 0
     for item in alphabet:
         if key[item] != alphabet[item]:
             diff = key[item] - alphabet[item]
         new_key[alphabet[item]] = item
-    print(diff)
-    print(new_key)
     for letter in message.lower():
         if letter.isalpha() == True:
             new_message += new_key[int((alphabet[letter])-diff)%26]
